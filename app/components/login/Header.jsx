a8e46694c5e98397b4a98a18ee520529668579f8
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-03 09:34:31 +0100

    Add a new Header component to Login page and Sign Up page

---
+++ app/components/login/Header.jsx
@@ -0,0 +1,11 @@
+import { LogoFull } from "@/app/lib/utils"
+
+const Header = () => {
+  return (
+    <header className='w-full bg-neutral-900 flex justify-center py-8'>
+      <LogoFull/>
+    </header>
+  )
+}
+
+export default Header;
