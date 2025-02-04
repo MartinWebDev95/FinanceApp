1f6be03d11d87bcfce4e1a78bd0254b2558d4399
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-04 09:11:12 +0100

    Change the Grid component to add a link to the specific page

--- app/components/dashboard/Grid.jsx
+++ app/components/dashboard/Grid.jsx
@@ -1,20 +1,47 @@
+import { ArrowDetails } from "@/app/lib/utils";
+import Link from "next/link";
+
 const Grid = () => {
   return (
-    <div className="mt-8 grid grid-cols-1 grid-rows-24 lg:grid-cols-2 lg:grid-rows-12 gap-4 h-screen">
-      <div className="bg-white rounded-md shadow-lg flex justify-center items-center row-start-1 row-end-6 lg:col-start-1 lg:col-end-2">
-        Pots
+    <div className="mt-10 lg:mt-8 grid grid-cols-1 grid-rows-24 lg:grid-cols-2 lg:grid-rows-12 gap-4 h-screen">
+      <div className="p-6 bg-white rounded-md shadow-lg row-start-1 row-end-6 lg:col-start-1 lg:col-end-2">
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-neutral-900 text-2xl font-bold">Pots</h2>
+          <Link href='/pots' className="flex items-center gap-2 group text-slate-600">
+            <span>See details</span>
+            <ArrowDetails />
+          </Link>
+        </div>
       </div>
 
-      <div className="bg-white rounded-md shadow-lg flex justify-center items-center row-start-12 row-end-19 lg:row-start-1 lg:row-end-8 lg:col-start-2 lg:col-end-3">
-        Budgets
+      <div className="p-6 bg-white rounded-md shadow-lg row-start-12 row-end-19 lg:row-start-1 lg:row-end-8 lg:col-start-2 lg:col-end-3">
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-neutral-900 text-2xl font-bold">Budgets</h2>
+          <Link href='/budgets' className="flex items-center gap-2 group text-slate-600">
+            <span>See details</span>
+            <ArrowDetails />
+          </Link>
+        </div>
       </div>
 
-      <div className="bg-white rounded-md shadow-lg flex justify-center items-center row-start-6 row-end-12 lg:row-end-13 lg:col-start-1 lg:col-end-2">
-        Transactions
+      <div className="p-6 bg-white rounded-md shadow-lg row-start-6 row-end-12 lg:row-end-13 lg:col-start-1 lg:col-end-2">
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-neutral-900 text-2xl font-bold">Transactions</h2>
+          <Link href='/transactions' className="flex items-center gap-2 group text-slate-600">
+            <span>See details</span>
+            <ArrowDetails />
+          </Link>
+        </div>
       </div>
 
-      <div className="bg-white rounded-md shadow-lg flex justify-center items-center row-start-19 row-end-25 lg:row-start-8 lg:row-end-13 lg:col-start-2 lg:col-end-3">
-        Recurring Bills
+      <div className="p-6 bg-white rounded-md shadow-lg row-start-19 row-end-25 lg:row-start-8 lg:row-end-13 lg:col-start-2 lg:col-end-3">
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-neutral-900 text-2xl font-bold">Recurring Bills</h2>
+          <Link href='/recurring-bills' className="flex items-center gap-2 group text-slate-600">
+            <span>See details</span>
+            <ArrowDetails />
+          </Link>
+        </div>
       </div>
     </div>
   )
