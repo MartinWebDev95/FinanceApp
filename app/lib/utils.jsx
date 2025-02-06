0390cd130acd3026250b80d9851c465b992eb08c
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-06 13:08:27 +0100

    Add two new icons and an array that return the different themes

--- app/lib/utils.jsx
+++ app/lib/utils.jsx
@@ -100,6 +100,39 @@ export function LogoutIcon() {
   );
 }
 
+export function AddIcon() {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="22"  height="22"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-plus group-hover:rotate-180">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 5l0 14" />
+      <path d="M5 12l14 0" />
+    </svg>
+  );
+}
+
+export function ArrowDownIcon() {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-caret-down">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z" />
+    </svg>
+  )
+}
+
+//Themes for CustomSelect
+export const themes = [
+  { nameColor: 'Green', color: '#1bf526' },
+  { nameColor: 'Purple', color: '#a909b8' },
+  { nameColor: 'Blue', color: '#0b16bd' },
+  { nameColor: 'Yellow', color: '#fce300' },
+  { nameColor: 'Cyan', color: '#00ffbf' },
+  { nameColor: 'Red', color: '#ff0000' },
+  { nameColor: 'Brown', color: '#81492d' },
+  { nameColor: 'Pink', color: '#f50589' },
+  { nameColor: 'Orange', color: '#ff5e00' },
+  { nameColor: 'Gray', color: '#7a716b' },
+];
+
 //Fonts
 export const ptSans = PT_Sans({ weight: ['400', '700'], subsets: ['latin'] });
 
