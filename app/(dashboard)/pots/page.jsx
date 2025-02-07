f4a958ce3d2e52a49ce1dae41f5a65a39be5c431
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-07 10:10:05 +0100

    Add the Pot components to the PotsPage

--- app/(dashboard)/pots/page.jsx
+++ app/(dashboard)/pots/page.jsx
@@ -1,4 +1,6 @@
 import BtnAddModal from "@/app/components/pots/BtnAddModal";
+import Pot from "@/app/components/pots/Pot";
+import data from '@/app/lib/data.json';
 
 export default function PotsPage(){
   return (
@@ -6,6 +8,18 @@ export default function PotsPage(){
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Pots</h1>
         <BtnAddModal />
+      </div>
+
+      <div className="grid grid-cols-auto-fill gap-4 mt-8">
+        {data.pots.map(pot => (
+          <Pot 
+            key={pot.name} 
+            name={pot.name} 
+            target={pot.target} 
+            total={pot.total} 
+            theme={pot.theme} 
+          />
+        ))}
       </div>
     </>
   )
