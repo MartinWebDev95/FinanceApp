8f02ef44131cf971e48d75cc1b4278b8a8ec1f44
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-07 10:13:02 +0100

    Add new icons and a function that calculate the percentage of the money saved

--- app/lib/utils.jsx
+++ app/lib/utils.jsx
@@ -102,7 +102,7 @@ export function LogoutIcon() {
 
 export function AddIcon() {
   return (
-    <svg  xmlns="http://www.w3.org/2000/svg"  width="22"  height="22"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-plus group-hover:rotate-180">
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-plus group-hover:rotate-180">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M12 5l0 14" />
       <path d="M5 12l14 0" />
@@ -117,6 +117,17 @@ export function ArrowDownIcon() {
       <path d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z" />
     </svg>
   )
+}
+
+export function PotMenuIcon() {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-dots-vertical">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    </svg>
+  );
 }
 
 //Themes for CustomSelect
@@ -145,4 +156,9 @@ export const getNavLinks = () => {
     { name: "Pots", href: "/pots", icon: <PotsIcon/> },
     { name: "Recurring Bills", href: "/recurring-bills", icon: <RecurringBillsIcon/> },
   ];
+};
+
+//Calculate Percentage
+export const calculatePercentage = ({ total, target }) => {
+  return ((total / target) * 100).toFixed(2);
 };
