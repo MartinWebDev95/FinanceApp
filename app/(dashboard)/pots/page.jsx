a73b4b0e0abf368fdb15bb04f75c5d844570705e
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-12 11:05:20 +0100

    Remove the BtnAddModal component from PotsPage and replace it with the BtnAddNewPot component

--- app/(dashboard)/pots/page.jsx
+++ app/(dashboard)/pots/page.jsx
@@ -1,4 +1,4 @@
-import BtnAddModal from "@/app/components/pots/BtnAddModal";
+import { BtnAddNewPot } from "@/app/components/pots/buttons";
 import Pot from "@/app/components/pots/Pot";
 import data from '@/app/lib/data.json';
 
@@ -7,17 +7,14 @@ export default function PotsPage(){
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Pots</h1>
-        <BtnAddModal />
+        <BtnAddNewPot />
       </div>
 
       <div className="grid grid-cols-auto-fill gap-4 mt-8">
         {data.pots.map(pot => (
           <Pot 
             key={pot.name} 
-            name={pot.name} 
-            target={pot.target} 
-            total={pot.total} 
-            theme={pot.theme} 
+            {...pot}
           />
         ))}
       </div>
