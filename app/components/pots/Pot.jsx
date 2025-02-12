fe39bacf90c77ac07754ca0fe559d186e403410d
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-12 11:24:38 +0100

    Change the Pot component to add two different states to handle the AddMoneyModal component and the WithdrawModal component

--- app/components/pots/Pot.jsx
+++ app/components/pots/Pot.jsx
@@ -1,47 +1,78 @@
-import { AddIcon, calculatePercentage, PotMenuIcon } from "@/app/lib/utils";
+'use client';
 
-const Pot = ({ name, target, total, theme }) => {
+import { calculatePercentage } from "@/app/lib/utils";
+import { BtnAddMoney, BtnMenuPot, BtnWithdraw } from "./buttons";
+import { useState } from "react";
+import AddMoneyModal from "./AddMoneyModal";
+import WithdrawModal from "./WithdrawModal";
+
+const Pot = ({ name, total, target, theme }) => {
+
+  const [isOpenedAddMoneyModal, setIsOpenedAddMoneyModal] = useState(false);
+  const [isOpenedWithdrawModal, setIsOpenedWithdrawModal] = useState(false);
+
   return (
-    <div className="bg-white rounded-md p-4 shadow-lg">
-      <div className="flex items-center justify-between">
-        <h2 className="flex items-center gap-2.5">
-          <span className="w-4 h-4 rounded-full" style={{backgroundColor: theme}}></span>
-          <span className="text-xl font-bold">{name}</span> 
-        </h2>
+    <>
+      <div className="bg-white rounded-md p-4 shadow-lg">
+        <div className="flex items-center justify-between relative">
+          <h2 className="flex items-center gap-2.5">
+            <span className="w-4 h-4 rounded-full" style={{ backgroundColor: theme }}></span>
+            <span className="text-xl font-bold">{name}</span> 
+          </h2>
 
-        <button type="button">
-          <PotMenuIcon />
-        </button>
-      </div>
-
-      <div className="mt-8">
-        <p className="w-full flex items-center justify-between">
-          <span className="text-gray-500 font-bold">Total Saved</span> 
-          <span className="font-bold text-3xl">${total}</span>
-        </p>
-
-        {/* Custom Progress Bar */}
-        <div className="w-full h-3 bg-gray-200 rounded-full mt-2">
-          <div className="h-3 rounded-full" style={{ backgroundColor: theme, width: `${calculatePercentage({ total, target })}%` }}></div>
+          <BtnMenuPot />
         </div>
 
-        <p className="w-full flex items-center justify-between text-gray-500 font-bold text-xs">
-          <span>{calculatePercentage({ total, target })}%</span>
-          <span>Target of ${target}</span>
-        </p>
+        <div className="mt-8">
+          <p className="w-full flex items-center justify-between">
+            <span className="text-gray-500 font-bold">Total Saved</span> 
+            <span className="font-bold text-3xl">${total}</span>
+          </p>
+
+          {/* Custom Progress Bar */}
+          <div className="w-full h-3 bg-gray-200 rounded-full mt-2">
+            <div 
+              className="h-3 rounded-full" 
+              style={{ 
+                backgroundColor: theme, 
+                width: `${calculatePercentage({ total, target })}%` 
+              }}
+            /> 
+          </div>
+
+          <p className="w-full flex items-center justify-between text-gray-500 font-bold text-xs">
+            <span>{calculatePercentage({ total, target })}%</span>
+            <span>Target of ${target}</span>
+          </p>
+        </div>
+
+        <div className="mt-8 flex items-center justify-center gap-4">
+          <BtnAddMoney setIsOpened={setIsOpenedAddMoneyModal} />
+
+          <BtnWithdraw setIsOpened={setIsOpenedWithdrawModal} />
+        </div>
       </div>
 
-      <div className="mt-8 flex items-center justify-center gap-4">
-        <button type="button" className="w-full flex items-center justify-center gap-1 bg-slate-200 rounded-md p-2 font-bold">
-          <AddIcon />
-          Add Money
-        </button>
+      {isOpenedAddMoneyModal && (
+        <AddMoneyModal 
+          isOpened={isOpenedAddMoneyModal} 
+          setIsOpened={setIsOpenedAddMoneyModal}
+          name={name} 
+          total={total}
+          target={target}
+        />
+      )}
 
-        <button type="button" className="w-full bg-slate-200 rounded-md p-2 font-bold">
-          Withdraw
-        </button>
-      </div>
-    </div>
+      {isOpenedWithdrawModal && (
+        <WithdrawModal 
+          isOpened={isOpenedWithdrawModal} 
+          setIsOpened={setIsOpenedWithdrawModal}
+          name={name} 
+          total={total}
+          target={target}
+        />
+      )}
+    </>
   )
 }
 
