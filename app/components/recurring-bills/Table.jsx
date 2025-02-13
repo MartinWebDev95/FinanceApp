1c8aa258433e5184a9c41befca302c1e1052920a
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-13 14:01:14 +0100

    Add a new Table component to show the recurring bills

---
+++ app/components/recurring-bills/Table.jsx
@@ -0,0 +1,34 @@
+const Table = ({ data }) => {
+  return (
+    <table className="w-full table-auto">
+      <thead className="text-left">
+        <tr className="text-gray-500 text-xs">
+          <th className="pb-4">
+            Bill Title
+          </th>
+          <th className="pb-4">
+            Due Date
+          </th>
+          <th className="pb-4">
+            Amount
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.transactions.filter((transaction) => transaction.recurring).map((bill, index) => (
+          <tr key={index} className="font-bold text-neutral-900 border-t border-gray-200">
+            <td className="flex items-center gap-2 py-4 text-xs md:text-base">
+              <img src="./assets/Logo-1.jpg" alt={bill.name} className="w-8 h-8 rounded-full" />
+              {bill.name}
+            </td>
+            <td className="font-normal text-gray-500 text-xs">{bill.date.split('T')[0]}</td>
+            <td className="text-xs md:text-base">${Math.abs(bill.amount)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table;
