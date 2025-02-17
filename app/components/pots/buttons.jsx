91e201659f050503edefcfe84275ef0035eeb61e
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-17 11:06:11 +0100

    Change the buttons.jsx file to add a new button to open the transaction modal

--- app/components/pots/buttons.jsx
+++ app/components/pots/buttons.jsx
@@ -4,6 +4,7 @@ import { AddIcon, PotMenuIcon } from "@/app/lib/utils";
 import { useState } from "react";
 import Menu from "./Menu";
 import AddPotModal from "./AddPotModal";
+import AddTransactionModal from "../transactions/AddTransactionModal";
 
 export function BtnMenuPot() {
 
@@ -66,5 +67,25 @@ export function BtnWithdraw({ setIsOpened }) {
     >
       Withdraw
     </button>
+  )
+}
+
+export function BtnAddNewTransaction() {
+
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <>
+      <button 
+        type="button" 
+        className="flex items-center gap-2 bg-neutral-900 text-white font-bold rounded-md shadow-lg p-2 hover:bg-neutral-800 transition-all ease-in-out duration-200 group"
+        onClick={() => setIsOpened(true)}
+      >
+        <AddIcon />
+        <span>Add New Transaction</span>
+      </button>
+
+      <AddTransactionModal isOpened={isOpened} setIsOpened={setIsOpened} />
+    </>
   )
 }
