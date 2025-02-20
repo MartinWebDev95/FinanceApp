03ef3398c34701783fca774fe13e804cd65c61e6
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-20 09:49:59 +0100

    Change the AddPotModal to differentiate between edit mode or creation mode

--- app/components/pots/AddPotModal.jsx
+++ app/components/pots/AddPotModal.jsx
@@ -1,3 +1,4 @@
+import { themes } from "@/app/lib/utils";
 import CustomSelect from "./CustomSelect";
 
 const AddPotModal = ({ isOpened, setIsOpened, edit = false }) => {
@@ -19,30 +20,51 @@ const AddPotModal = ({ isOpened, setIsOpened, edit = false }) => {
     >
       <form action="" method="dialog" className="bg-white text-gray-600 p-6 rounded-md w-96 shadow-xl">
         <fieldset>
-          <legend className="mb-2 text-neutral-900 font-bold text-2xl">Add New Pot</legend>
+          <legend className="mb-2 text-neutral-900 font-bold text-2xl">
+            { edit ? 'Edit Pot' : 'Add New Pot' }
+          </legend>
 
           <p className="mb-4 text-sm w-full">
-            Choose a category to set a spending budget. These categories can help you monitor spending.
+            {
+              edit 
+                ? 'If your saving targets change, feel free to update your pots.'
+                : 'Choose a category to set a spending budget. These categories can help you monitor spending.'
+            }
           </p>
 
           <div className="mb-3">
             <label htmlFor="potName">Pot Name</label>
-            <input className="border border-gray-400 rounded-md block mt-1 py-1 px-2 w-full" type="text" name="potName" id="potName" placeholder="e.g. Rainy Days" />
+            <input 
+              className="border border-gray-400 rounded-md block mt-1 py-1 px-2 w-full" 
+              type="text" 
+              name="potName" 
+              id="potName" 
+              placeholder="e.g. Rainy Days" 
+            />
           </div>
 
           <div className="mb-3">
             <label htmlFor="targetAmount">Target Amount</label>
-            <input className="border border-gray-400 rounded-md block mt-1 py-1 px-2 w-full" type="number" name="targetAmount" id="targetAmount" placeholder="$ e.g. 2000" />
+            <input 
+              className="border border-gray-400 rounded-md block mt-1 py-1 px-2 w-full" 
+              type="number" 
+              name="targetAmount" 
+              id="targetAmount" 
+              placeholder="$ e.g. 2000" 
+            />
           </div>
 
           <div>
-            <label htmlFor="targetAmount">Theme</label>
-            <CustomSelect />
+            <label htmlFor="targetTheme">Theme</label>
+            <CustomSelect data={themes} placeholder="Select a theme" />
           </div>
         </fieldset>
 
-        <button type="submit" className="w-full rounded-md bg-neutral-900 text-white font-bold mt-8 py-2 hover:bg-neutral-800 transition-all ease-in-out duration-200">
-          Add New Pot
+        <button 
+          type="submit" 
+          className="w-full rounded-md bg-neutral-900 text-white font-bold mt-8 py-2 hover:bg-neutral-800 transition-all ease-in-out duration-200"
+        >
+          { edit ? 'Save Changes' : 'Add New Pot' }
         </button>
       </form>
     </dialog>
