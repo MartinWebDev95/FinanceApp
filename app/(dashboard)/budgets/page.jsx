494449d7916172da71d573585ef3b4f9704c7d34
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-20 09:46:33 +0100

    Change the BudgetsPage to show the different budgets and a spending summary

--- app/(dashboard)/budgets/page.jsx
+++ app/(dashboard)/budgets/page.jsx
@@ -1,5 +1,50 @@
+import Budget from "@/app/components/budgets/Budget";
+import { BtnAddNewBudget } from "@/app/components/pots/buttons";
+import data from '@/app/lib/data.json';
+
 export default function BudgetsPage(){
+
   return (
-    <div>BudgetsPage</div>
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Budgets</h1>
+        <BtnAddNewBudget />
+      </div>
+
+      <div className="flex flex-col lg:flex-row items-start gap-4 mt-8">
+        <div className="w-full lg:w-2/5 bg-white rounded-md shadow-lg p-4">
+          <p className="text-neutral-900 mb-4 text-lg font-bold">Spending Summary</p>
+          <ul>
+            {
+              data.budgets.map((budget, index) => (
+                <li key={index} className="py-4 border-b relative">
+                  <div className="absolute top-0 left-0 w-2 h-full rounded-full" style={{ backgroundColor: budget.theme }} />
+
+                  <div className="flex items-center justify-between pl-4">
+                    <p className="font-bold text-neutral-900">{budget.category}</p>
+                    <p className="text-slate-500">
+                      <span className="font-bold text-neutral-900">
+                        $5363
+                      </span>
+                      {' '}
+                      of
+                      {' '}
+                      <span>
+                        ${budget.maximum}
+                      </span>         
+                    </p>               
+                  </div>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="w-full lg:w-3/5">
+          {data.budgets.map((budget, index) => (
+            <Budget key={index} budget={budget} data={data} />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
