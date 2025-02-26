e4f2988b1b0f85808db0549844f62e26837f64d6
Juan Antonio Martín <jamll.muro@gmail.com>
2025-02-26 10:58:54 +0100

    Add a new server action to create a user in the database

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -22,3 +22,26 @@ export async function login(prevState, formData) {
   // Redirect to the dashboard
   redirect('/');
 }
+
+export async function createUser(prevState, formData) {
+  const rawFormData = {
+    username: formData.get('username'),
+    email: formData.get('email'),
+    password: formData.get('password'),
+  };
+
+  const { username, email, password } = rawFormData;
+
+  // Hash the password
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  try {
+    await sql`INSERT INTO users (name, email, password) VALUES (${username}, ${email}, ${passwordHash})`;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to create user.');
+  }
+
+  // Redirect to the login page
+  redirect('/login');
+}
