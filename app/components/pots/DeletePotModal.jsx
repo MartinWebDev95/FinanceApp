f49e5bb8df4067a728f80ab6a36d33d60ac9d9cf
Juan Antonio Martín <jamll.muro@gmail.com>
2025-03-03 11:53:42 +0100

    Change the DeletePotModal component to delete the pot

--- app/components/pots/DeletePotModal.jsx
+++ app/components/pots/DeletePotModal.jsx
@@ -1,6 +1,9 @@
+import { deletePot } from '@/app/lib/actions';
 import React from 'react'
 
-const DeletePotModal = ({ isOpened, setIsOpened}) => {
+const DeletePotModal = ({ isOpened, setIsOpened, id, name}) => {
+
+  const deletePotById = deletePot.bind(null, id);
 
   const handleCloseModal = (e) => {
     if(e.target.ariaModal){
@@ -12,9 +15,11 @@ const DeletePotModal = ({ isOpened, setIsOpened}) => {
 
   return (
     <dialog id="deletePotDialog" aria-modal className={`absolute top-0 left-0 w-full h-screen bg-black/70 ${isOpened ? 'grid place-items-center' : 'hidden'} z-10`} onClick={handleCloseModal}>
-      <form action="" method="dialog" className="bg-white text-gray-600 p-6 rounded-md w-96 shadow-xl">
+      <form action={deletePotById} className="bg-white text-gray-600 p-6 rounded-md w-96 shadow-xl">
         <fieldset>
-          <legend className="mb-2 text-neutral-900 font-bold text-2xl">Delete 'Savings'?</legend>
+          <legend className="mb-2 text-neutral-900 font-bold text-2xl">
+            Delete '{`${name}`}'?
+          </legend>
 
           <p className="mb-4 text-sm w-full">
             Are you sure you want to delete this pot? This action cannot be reversed, and all the data inside it will be removed forever.
@@ -23,8 +28,7 @@ const DeletePotModal = ({ isOpened, setIsOpened}) => {
           <div className="mt-4 w-full flex items-center justify-end gap-4">
             <button 
               type="submit" 
-              className="w-fit rounded-md bg-red-600 text-white font-bold p-3 hover:bg-red-500 transition-all ease-in-out duration-200"
-              onClick={() => setIsOpened(false)}  
+              className="w-fit rounded-md bg-red-600 text-white font-bold p-3 hover:bg-red-500 transition-all ease-in-out duration-200" 
             >
               Yes, Confirm Deletion
             </button>
