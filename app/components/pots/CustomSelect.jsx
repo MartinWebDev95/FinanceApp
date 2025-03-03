cbe2e0708cfc5e47edd427c017fd49027713b214
Juan Antonio Martín <jamll.muro@gmail.com>
2025-03-03 12:46:58 +0100

    Change the CustomSelect component to receive by props the default value

--- app/components/pots/CustomSelect.jsx
+++ app/components/pots/CustomSelect.jsx
@@ -1,8 +1,10 @@
-import { ArrowDownIcon, themes } from '@/app/lib/utils';
+import { ArrowDownIcon } from '@/app/lib/utils';
 import { useEffect, useRef, useState } from 'react'
 
-const CustomSelect = ({ data, placeholder }) => {
-  const [selected, setSelected] = useState({ label: placeholder, value: '' });
+const CustomSelect = ({ data, defaultValue, placeholder }) => {
+  const [selected, setSelected] = useState (
+    data.find(option => option.value === defaultValue) || { label: placeholder, value: '' }
+  );
   const [isOpen, setIsOpen] = useState(false);
   const selectRef = useRef(null);
 
@@ -28,7 +30,7 @@ const CustomSelect = ({ data, placeholder }) => {
       >
         <p className='flex items-center gap-2'>
           {selected.value && (
-            <span className={`w-3 h-3 rounded-full`} style={{ backgroundColor: selected.value }}/>
+            <span className="w-3 h-3 rounded-full" style={{ backgroundColor: selected.value }}/>
           )}
 
           <span>{selected.label}</span>
@@ -49,7 +51,7 @@ const CustomSelect = ({ data, placeholder }) => {
                 setIsOpen(false);
               }}
             >
-              <span className={`w-3 h-3 rounded-full`} style={{ backgroundColor: option.value }}/>
+              <span className="w-3 h-3 rounded-full" style={{ backgroundColor: option.value }}/>
 
               {option.label}
             </li>
