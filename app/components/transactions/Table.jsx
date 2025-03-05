0bc3bbacc22275521bcab421bd6bf45547096bc7
Juan Antonio Martín <jamll.muro@gmail.com>
2025-03-05 12:27:41 +0100

    Change the Table component with the data obtained from the database

--- app/components/transactions/Table.jsx
+++ app/components/transactions/Table.jsx
@@ -19,17 +19,17 @@ const Table = ({ data }) => {
       </thead>
 
       <tbody>
-        {data.map((transaction, index) => (
-          <tr key={index} className="font-bold text-neutral-900 border-t border-gray-200">
+        {data.map(transaction => (
+          <tr key={transaction.id} className="font-bold text-neutral-900 border-t border-gray-200">
             <td className="flex items-center gap-2 py-4 text-xs md:text-base">
-              <img src="./assets/Logo-1.jpg" alt={transaction.name} className="w-8 h-8 rounded-full" />
+              <img src={transaction.avatar} alt={transaction.name} className="w-8 h-8 rounded-full" />
               {transaction.name}
             </td>
             <td className="font-normal text-gray-500 text-xs">
-              {transaction.category}
+              {transaction.label}
             </td>
             <td className="font-normal text-gray-500 text-xs">
-              {transaction.date.split('T')[0]}
+              {new Date(transaction.date).toLocaleDateString()}
             </td>
             {transaction.amount > 0 ? (
               <td className="text-xs md:text-base text-green-600">
