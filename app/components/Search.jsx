8dae16d5b8ff115d0442eb1f086004b7e32f19f6
Juan Antonio Martín <jamll.muro@gmail.com>
2025-03-05 14:29:18 +0100

    Change the Search component to update the url with the param to search

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,19 +1,33 @@
 'use client';
 
-const Search = ({ placeholder }) => {
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
+const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((e) => {
+    const params = new URLSearchParams(searchParams);
+
+    if(e.target.value){
+      params.set('search', e.target.value);
+    }else{
+      params.delete('search');
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
-    <form action="" onSubmit={handleSubmit} className="w-80">
-      <input 
-        type="text" 
-        placeholder={placeholder} 
-        className="border border-gray-400 rounded-md p-2 w-full" 
-      />
-    </form>
+    <input 
+      type="text" 
+      placeholder={placeholder}
+      defaultValue={searchParams.get('search')?.toString()}
+      onChange={handleSearch} 
+      className="border border-gray-400 rounded-md p-2 w-full" 
+    />
   )
 }
 
