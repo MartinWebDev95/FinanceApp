67b94d687cf6e3b2d6417a45630eaaf03778ffa6
Juan Antonio <jamll.muro@gmail.com>
2025-03-10 10:05:52 +0100

    Change the Table component to call the fetchRecurringBills function inside it with the query prop

--- app/components/recurring-bills/Table.jsx
+++ app/components/recurring-bills/Table.jsx
@@ -1,4 +1,10 @@
-const Table = ({ data }) => {
+import { fetchRecurringBills } from "@/app/lib/data";
+import { recurringBillsDateFormatter } from "@/app/lib/utils";
+
+const Table = async ({ query }) => {
+
+  const recurringBills = await fetchRecurringBills({ query });
+
   return (
     <table className="w-full table-auto">
       <thead className="text-left">
@@ -16,13 +22,15 @@ const Table = ({ data }) => {
       </thead>
 
       <tbody>
-        {data.transactions.filter((transaction) => transaction.recurring).map((bill, index) => (
-          <tr key={index} className="font-bold text-neutral-900 border-t border-gray-200">
+        {recurringBills.map(bill => (
+          <tr key={bill.id} className="font-bold text-neutral-900 border-t border-gray-200">
             <td className="flex items-center gap-2 py-4 text-xs md:text-base">
               <img src="./assets/Logo-1.jpg" alt={bill.name} className="w-8 h-8 rounded-full" />
               {bill.name}
             </td>
-            <td className="font-normal text-gray-500 text-xs">{bill.date.split('T')[0]}</td>
+            <td className="font-normal text-gray-500 text-xs">
+              {recurringBillsDateFormatter({ date: bill.date })}
+            </td>
             <td className="text-xs md:text-base">${Math.abs(bill.amount)}</td>
           </tr>
         ))}
