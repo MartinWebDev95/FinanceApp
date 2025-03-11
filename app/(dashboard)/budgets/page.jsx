00d8eecffd88c48d8d428f948db1974471cecdb3
Juan Antonio <jamll.muro@gmail.com>
2025-03-11 13:44:59 +0100

    Change the BudgetsPage to fetch the budgets

--- app/(dashboard)/budgets/page.jsx
+++ app/(dashboard)/budgets/page.jsx
@@ -1,10 +1,10 @@
 import Budget from "@/app/components/budgets/Budget";
 import { BtnAddNewBudget } from "@/app/components/pots/buttons";
-import { fetchCategories } from "@/app/lib/data";
-import data from '@/app/lib/data.json';
+import { fetchBudgets, fetchCategories } from "@/app/lib/data";
 
 export default async function BudgetsPage() {
 
+  const budgets = await fetchBudgets();
   const categories = await fetchCategories();
 
   return (
@@ -19,15 +19,18 @@ export default async function BudgetsPage() {
           <p className="text-neutral-900 mb-4 text-lg font-bold">Spending Summary</p>
           <ul>
             {
-              data.budgets.map((budget, index) => (
-                <li key={index} className="py-4 border-b relative">
-                  <div className="absolute top-0 left-0 w-2 h-full rounded-full" style={{ backgroundColor: budget.theme }} />
+              budgets.map(budget => (
+                <li key={budget.id} className="py-4 border-b relative">
+                  <div 
+                    className="absolute top-0 left-0 w-2 h-full rounded-full" 
+                    style={{ backgroundColor: budget.theme }} 
+                  />
 
                   <div className="flex items-center justify-between pl-4">
-                    <p className="font-bold text-neutral-900">{budget.category}</p>
+                    <p className="font-bold text-neutral-900">{budget.label}</p>
                     <p className="text-slate-500">
                       <span className="font-bold text-neutral-900">
-                        $5363
+                        ${Math.abs(budget.total_transactions_amount)}
                       </span>
                       {' '}
                       of
@@ -42,9 +45,10 @@ export default async function BudgetsPage() {
             }
           </ul>
         </div>
+        
         <div className="w-full lg:w-3/5">
-          {data.budgets.map((budget, index) => (
-            <Budget key={index} budget={budget} data={data} />
+          {budgets.map(budget => (
+            <Budget key={budget.id} budget={budget} />
           ))}
         </div>
       </div>
