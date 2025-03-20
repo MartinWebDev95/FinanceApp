e09f766275b0f5cced1220e5fa38fc0e44169976
Juan Antonio <jamll.muro@gmail.com>
2025-03-20 14:36:43 +0100

    Change the RecurringBillsPage to add the Summary component with the Suspense component and its skeleton and the same for the Table component

--- app/(dashboard)/recurring-bills/page.jsx
+++ app/(dashboard)/recurring-bills/page.jsx
@@ -1,45 +1,23 @@
-import { RecurringBillsIcon, sortBy } from "@/app/lib/utils";
+import { sortBy } from "@/app/lib/utils";
 import Table from "@/app/components/recurring-bills/Table";
 import Search from "@/app/components/Search";
 import Filter from "@/app/components/Filter";
-import { fetchBillsSummary } from "@/app/lib/data";
+import { RecurringBillsTableSkeleton, SummarySkeleton } from "@/app/components/skeletons";
+import { Suspense } from "react";
+import Summary from "@/app/components/recurring-bills/Summary";
 
 export default async function RecurringBillsPage({ searchParams }){
 
   const query = await searchParams;
-  const { totalBills, paidBills, upcomingBills, dueSoon } = await fetchBillsSummary();
 
   return (
     <>
       <h1 className="text-3xl font-bold">Recurring Bills</h1>
 
       <div className="flex flex-col lg:flex-row lg:items-start gap-4 mt-8">
-        <div className="w-full lg:w-1/4 flex flex-col md:flex-row lg:flex-col md:gap-4">
-          <div className="bg-neutral-900 text-white p-4 rounded-md shadow-lg mb-4 md:mb-0 w-full min-h-full flex flex-col justify-center">
-            <RecurringBillsIcon/>
-
-            <div>
-              <p className="mt-4 mb-2 font-bold text-lg">Total Bills</p>
-              <p className="font-bold text-4xl">${Math.abs(totalBills.sum)}</p>
-            </div>
-          </div>
-
-          <div className="bg-white p-4 rounded-md shadow-lg text-xs w-full h-full">
-            <p className="text-neutral-900 mb-4 text-lg font-bold">Summary</p>
-            <p className="pb-4 w-full border-b border-gray-200 flex items-center justify-between">
-              <span className="text-gray-500">Paid Bills</span>
-              <span>${Math.abs(paidBills.sum)}</span>
-            </p>
-            <p className="py-4 w-full border-b border-gray-200 flex items-center justify-between">
-              <span className="text-gray-500">Total Upcoming</span>
-              <span>${Math.abs(upcomingBills.sum)}</span>
-            </p>
-            <p className="pt-4 w-full flex items-center justify-between">
-              <span className="text-gray-500">Due Soon</span>
-              <span className="text-red-600">${Math.abs(dueSoon.sum)}</span>
-            </p>
-          </div>
-        </div>
+        <Suspense fallback={<SummarySkeleton />}>
+          <Summary />
+        </Suspense>
 
         <div className="w-full lg:w-3/4 h-full bg-white p-8 rounded-md shadow-lg">
           <div className="w-full flex justify-between items-center pb-8 gap-4">
@@ -50,8 +28,10 @@ export default async function RecurringBillsPage({ searchParams }){
               <Filter options={sortBy} type='sort' />
             </div>
           </div>
-          
-          <Table query={query} />
+ 
+          <Suspense fallback={<RecurringBillsTableSkeleton />}>
+            <Table query={query} />
+          </Suspense>
         </div>
       </div>
     </>
