50243c3c3121e9daa99f10ae7594af413e2e3243
Juan Antonio <jamll.muro@gmail.com>
2025-03-24 09:13:07 +0100

    Change the BudgetsPage to add the Suspense component and its skeletons to the BudgetsSummary and Budget components

--- app/(dashboard)/budgets/page.jsx
+++ app/(dashboard)/budgets/page.jsx
@@ -1,31 +1,36 @@
 import Budget from "@/app/components/budgets/Budget";
 import BudgetsSummary from "@/app/components/budgets/BudgetsSummary";
 import { BtnAddNewBudget } from "@/app/components/pots/buttons";
+import { BudgetSkeleton, BudgetSummarySkeleton } from "@/app/components/skeletons";
 import { fetchBudgets, fetchCategories } from "@/app/lib/data";
+import { Suspense } from "react";
 
 export default async function BudgetsPage() {
 
-  const budgets = await fetchBudgets();
-  const categories = await fetchCategories();
+  const data = await Promise.all([fetchBudgets(), fetchCategories()]);
 
   return (
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Budgets</h1>
-        <BtnAddNewBudget categories={categories} />
+        <BtnAddNewBudget categories={data[1]} />
       </div>
 
       <div className="flex flex-col lg:flex-row items-start gap-4 mt-8">
-        <div className="w-full lg:w-2/5 bg-white rounded-md shadow-lg p-4">
-          <p className="text-neutral-900 mb-4 text-lg font-bold">Spending Summary</p>
-          <BudgetsSummary budgets={budgets} />
-        </div>
+        <Suspense fallback={<BudgetSummarySkeleton />}>
+          <div className="w-full lg:w-2/5 bg-white rounded-md shadow-lg p-4">
+            <p className="text-neutral-900 mb-4 text-lg font-bold">Spending Summary</p>
+            <BudgetsSummary budgets={data[0]} />
+          </div>
+        </Suspense>
         
-        <div className="w-full lg:w-3/5">
-          {budgets.map(budget => (
-            <Budget key={budget.id} budget={budget} categories={categories} />
-          ))}
-        </div>
+        <Suspense fallback={<BudgetSkeleton />}>
+          <ul className="w-full lg:w-3/5">
+            {data[0].map(budget => (
+              <Budget key={budget.id} budget={budget} categories={data[1]} />
+            ))}
+          </ul>
+        </Suspense>
       </div>
     </>
   )
