d22a495f6791a66bc7960e38c90d12c3e5003e5c
Juan Antonio <jamll.muro@gmail.com>
2025-03-24 09:08:44 +0100

    Change the div element to a li element in Budget component

--- app/components/budgets/Budget.jsx
+++ app/components/budgets/Budget.jsx
@@ -4,11 +4,11 @@ import BudgetInfo from "./BudgetInfo";
 const Budget = ({ budget, categories }) => {
 
   return (
-    <div className="bg-white rounded-md shadow-lg p-6 mb-6">
+    <li className="bg-white rounded-md shadow-lg p-6 mb-6">
       <BudgetInfo budget={budget} categories={categories} />
 
       <LatestSpending budget={budget} />
-    </div>
+    </li>
   )
 }
 
