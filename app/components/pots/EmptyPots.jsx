1bfc1263adb5f3daddf25e63609ba93914357f16
Juan Antonio <jamll.muro@gmail.com>
2025-03-28 12:56:01 +0100

    Add a new EmptyPots component to show a message when in the PotsPage there are not pots created

---
+++ app/components/pots/EmptyPots.jsx
@@ -0,0 +1,11 @@
+const EmptyPots = () => {
+  return (
+    <div className="bg-white w-full p-8 mt-6 shadow-lg rounded-md">
+      <p className='text-center text-slate-500'>
+        There are not pots created at the moment.
+      </p>
+    </div>
+  )
+}
+
+export default EmptyPots
--- app/components/pots/ListOfPots.jsx
+++ app/components/pots/ListOfPots.jsx
@@ -1,9 +1,12 @@
+import EmptyPots from './EmptyPots';
 import Pot from './Pot';
 import { fetchPots } from '@/app/lib/data';
 
 const ListOfPots = async () => {
 
   const pots = await fetchPots();
+
+  if(pots.length === 0) return <EmptyPots />
 
   return (
     <ul className="grid grid-cols-auto-fill gap-4 mt-8">
