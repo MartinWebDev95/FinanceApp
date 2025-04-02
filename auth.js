ced0f9daa27ee410af47ea534362f54ba88f5a3e
Juan Antonio <jamll.muro@gmail.com>
2025-04-02 09:33:38 +0200

    Remove  error checks because they are checked in the server action login

--- auth.js
+++ auth.js
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
 import { getUser } from './app/lib/data';
 
 export const { auth, signIn, signOut } = NextAuth({
@@ -9,21 +8,8 @@ export const { auth, signIn, signOut } = NextAuth({
   providers: [
     Credentials({
       async authorize(credentials) {
-        const { email, password } = credentials;
-
-        // Get the user with the email
-        const user = await getUser(email);
-        
-        // If the user does not exist, return null
-        if (!user) return null;
-        
-        // Compare the password with the hashed password from the database
-        const passwordsMatch = await bcrypt.compare(password, user.password);
-
-        // If the passwords match, return the user
-        if (passwordsMatch) return user;
-
-        return null;
+        const user = await getUser(credentials.email);
+        return user;
       }
     }),
   ],
