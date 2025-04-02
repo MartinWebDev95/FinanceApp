e79a4cba725ca100ccfebd8a509a6c5a34e4068b
Juan Antonio <jamll.muro@gmail.com>
2025-04-02 10:46:35 +0200

    Change the SignUpPage to display errors if there are

--- app/(login)/sign-up/page.jsx
+++ app/(login)/sign-up/page.jsx
@@ -14,22 +14,65 @@ export default function SignUpPage(){
         <legend className="mb-4 text-neutral-900 font-bold text-4xl">Sign Up</legend>
 
         <div className="mb-3">
-          <label htmlFor="email">Name</label>
-          <input className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" type="text" name="username" id="username" />
+          <label htmlFor="username">Name</label>
+          <input 
+            type="text" 
+            name="username" 
+            id="username" 
+            className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" 
+          />
+
+          {
+            errorMessage?.errors?.username && (
+              errorMessage.errors.username.map((msg, index) => (
+                <p key={index} className="text-red-500 text-sm">{msg}</p>
+              ))
+            )
+          }
         </div>
 
         <div className="mb-3">
           <label htmlFor="email">Email</label>
-          <input className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" type="email" name="email" id="email" />
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" 
+          />
+
+          {
+            errorMessage?.errors?.email && (
+              errorMessage.errors.email.map((msg, index) => (
+                <p key={index} className="text-red-500 text-sm">{msg}</p>
+              ))
+            )
+          }
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
-          <input className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" type="password" name="password" id="password" />
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            className="border border-gray-400 rounded-md block mt-1 py-0.5 px-2 w-full" 
+          />
+
+          {
+            errorMessage?.errors?.password && (
+              errorMessage.errors.password.map((msg, index) => (
+                <p key={index} className="text-red-500 text-sm">{msg}</p>
+              ))
+            )
+          }
         </div>
       </fieldset>
 
-      <button type="submit" className="w-full rounded-md bg-neutral-900 text-white font-bold mt-8 py-2 hover:bg-neutral-800 transition-all ease-in-out duration-200" disabled={isPending}>
+      <button 
+        type="submit" 
+        aria-disabled={isPending}
+        className="w-full rounded-md bg-neutral-900 text-white font-bold mt-8 py-2 hover:bg-neutral-800 transition-all ease-in-out duration-200 aria-disabled:cursor-not-allowed aria-disabled:opacity-70" 
+      >
         Sign Up
       </button>
 
