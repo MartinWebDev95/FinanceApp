9ce7bd9de8472b36862a8c075c64313c68457296
Juan Antonio Martín <jamll.muro@gmail.com>
2025-06-25 14:18:48 +0200

    Add a new CSS class to change the scrollbar width and colour

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scrollbar">
       <body className={`${ptSans.className} antialiased overflow-hidden h-screen bg-slate-200`}>
         {children}
       </body>
