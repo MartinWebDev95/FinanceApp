2bc68ccfbe565c3203c40db072ed5cb4792bcbd9
Juan Antonio Martín <jamll.muro@gmail.com>
2025-06-25 14:47:02 +0200

    Change some styles to add a transition when the sidebar is opened or closed

--- app/components/dashboard/NavLinks.jsx
+++ app/components/dashboard/NavLinks.jsx
@@ -7,18 +7,18 @@ const NavLinks = ({ isMinimize }) => {
   const pathname = usePathname();
 
   return (
-    <nav className={`${isMinimize ? 'lg:w-fit' : 'lg:w-52'} w-full flex flex-row justify-evenly lg:flex-col`}>
+    <nav className={`${isMinimize ? 'lg:w-14' : 'lg:w-52'} w-full flex flex-row justify-evenly lg:flex-col lg:transition-all lg:duration-300 lg:ease-in-out lg:overflow-hidden`}>
       {getNavLinks().map((link) => (
         <Link 
           key={link.name} 
           href={link.href} 
-          className={`relative group flex flex-col lg:flex-row items-center gap-2 py-4 md:py-3 px-5 md:px-6 lg:py-5 lg:px-4 transition-all ease-in-out duration-200 ${isMinimize ? 'lg:rounded-none' : 'lg:rounded-r-lg'} ${pathname === link.href ? 'bg-white text-black after:block after:absolute after:left-0 lg:after:top-0 after:bottom-0 after:bg-emerald-700 lg:after:w-1 after:w-full lg:after:h-full after:h-1' : 'bg-transparent text-gray-400 hover:text-white'}`}
+          className={`relative whitespace-nowrap group flex flex-col lg:flex-row items-center gap-2 py-4 md:py-3 px-5 md:px-6 lg:py-5 lg:px-4 transition-all ease-in-out duration-200 ${isMinimize ? 'lg:rounded-none' : 'lg:rounded-r-lg'} ${pathname === link.href ? 'bg-white text-black after:block after:absolute after:left-0 lg:after:top-0 after:bottom-0 after:bg-emerald-700 lg:after:w-1 after:w-full lg:after:h-full after:h-1' : 'bg-transparent text-gray-400 hover:text-white'}`}
         >
           <div className={`${pathname === link.href ? 'text-emerald-700' : 'text-gray-400 group-hover:text-white transition-all ease-in-out duration-200'}`}>
             {link.icon} 
           </div>
 
-          <span className={`${isMinimize ? 'lg:hidden' : 'lg:block'} hidden md:block text-sm lg:text-base`}>{link.name}</span>
+          <span className={`${isMinimize ? 'lg:opacity-0' : 'lg:opacity-100'} hidden md:block text-sm lg:text-base lg:transition-opacity lg:duration-150 lg:ease-linear`}>{link.name}</span>
         </Link>
       ))}
     </nav>
--- app/components/dashboard/Sidenav.jsx
+++ app/components/dashboard/Sidenav.jsx
@@ -9,7 +9,7 @@ const Sidenav = () => {
   const [isMinimize, setIsMinimize] = useState(false);
 
   return (
-    <aside className={`${isMinimize ? 'lg:w-fit' : 'lg:w-72'} w-full bg-neutral-900 text-gray-400 h-fit lg:h-screen rounded-none lg:rounded-r-lg flex flex-col justify-between items-start transition-all ease-in-out duration-300 shadow-xl order-2 lg:order-1 font-bold sticky bottom-0 left-0 lg:top-0 z-30`}>
+    <aside className={`${isMinimize ? 'lg:w-14' : 'lg:w-72'} w-full bg-neutral-900 text-gray-400 h-fit lg:h-screen rounded-none lg:rounded-r-lg flex flex-col justify-between items-start transition-all ease-in-out duration-300 shadow-xl order-2 lg:order-1 font-bold sticky bottom-0 left-0 lg:top-0 z-30 overflow-hidden`}>
       <div className='w-full'>
         <div className='my-8 pl-4 hidden lg:block'>
           {isMinimize ? <LogoMinimize /> : <LogoFull />}
@@ -26,7 +26,7 @@ const Sidenav = () => {
       >
         <ArrowIcon isMinimize={isMinimize} />
 
-        {!isMinimize && <span>Minimize Menu</span>}
+        <span className={`${isMinimize ? 'opacity-0' : 'opacity-100'} transition-all duration-150 ease-linear whitespace-nowrap`}>Minimize Menu</span>
       </button>
     </aside>
   )
